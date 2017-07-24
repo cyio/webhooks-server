4cb413a492e379ba5cb045827cc9272c0c1f3433
cyio <icaner@qq.com>
2017-07-24 18:10:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+* 支持为多个项目配置 webhook，请查看 `config.json`
+* 运行 `npm start`
+* 建议搭配 pm2 使用 `pm2 start index.js --watch --name 'webhooks-server'`
---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const exec = require('child_process').exec;
+const config = require('./config.json');
+const server = http.createServer(function(req, res){
+  serverResponse(req, res)
+});
+server.listen(config.port, config.hostname, function(){
+  console.log(`Server running at http://${config.hostname}:${config.port}/`);
+});
+
+function serverResponse(req, res) {
+  const url = req.url
+  let rules = {
+    'default': function() {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 error!");
+    }
+  }
+  config.projects.forEach(function(rule){
+    rules[rule.url] = function() {
+      exec(rule.script, {cwd: rule.cwd}, function(err,stdout){
+        console.log(err, stdout);
+      });
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(rule.url + ' OK');
+    }
+  })
+  return rules.hasOwnProperty(url) ? rules[url]() : rules['default']()
+}
